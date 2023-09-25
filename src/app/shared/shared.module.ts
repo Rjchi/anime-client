5cb98615d4e790f23979a77a23a799000352973c
Rjchi <richardospina18@gmail.com>
2023-09-24 21:28:22 -0500

    [Feat] Agregar spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [FooterComponent, NavigationComponent],
+  declarations: [FooterComponent, NavigationComponent, SpinnerComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavigationComponent, FooterComponent],
+  exports: [NavigationComponent, FooterComponent, SpinnerComponent],
 })
 export class SharedModule {}
